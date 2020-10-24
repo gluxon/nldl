32529bd8fde1f7d526b13228189985242da72b42
Brandon Cheng <brandon.cheng@protonmail.com>
2020-10-23 20:21:50 -0400

    Replace get_acpi_event_id with get_nlctrl_id test
    The `acpi_event` family id changed from Ubuntu 16.04 to Ubuntu 18.04. It
    went from 23 to 24, which makes this test difficult to keep universal.
    
    The `nlctrl` family ID should be more stable, so switching this test to
    that.

--- netlink15_genl/tests/get_family.rs
+++ netlink15_genl/tests/get_family.rs
@@ -3,13 +3,13 @@ use netlink15_genl::ctrl::NetlinkGenericController;
 use netlink15_genl::socket::GenlSocket;
 
 #[test]
-fn get_acpi_event_id() -> anyhow::Result<()> {
+fn get_nlctrl_id() -> anyhow::Result<()> {
     let genl_controller = GenlSocket::connect()?;
-    let attrs = genl_controller.get_family("acpi_event".to_string())?;
+    let attrs = genl_controller.get_family("nlctrl".to_string())?;
     let family_id_attr = attrs
         .iter()
         .find(|attr| matches!(attr, ControllerAttribute::FamilyId(_)));
 
-    assert_eq!(family_id_attr, Some(&ControllerAttribute::FamilyId(0x18)));
+    assert_eq!(family_id_attr, Some(&ControllerAttribute::FamilyId(0x10)));
     Ok(())
 }
