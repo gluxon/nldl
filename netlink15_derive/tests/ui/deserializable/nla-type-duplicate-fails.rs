23b7002df37d293babaf38c269985e2e86f3b0f2
Brandon Cheng <gluxon@users.noreply.github.com>
2021-12-26 00:57:52 -0500

    Add UnknownAttribute to deserializable test cases

--- netlink15_derive/tests/ui/deserializable/nla-type-duplicate-fails.rs
+++ netlink15_derive/tests/ui/deserializable/nla-type-duplicate-fails.rs
@@ -1,6 +1,7 @@
 use netlink15_derive::NetlinkAttributeDeserializable;
 use netlink15_derive::NetlinkAttributeSerializable;
 use netlink15_core::utils::ParseNlaIntError;
+use netlink15_core::attr::UnknownAttribute;
 
 const ZERO: u16 = 0;
 
@@ -11,6 +12,8 @@ enum ControllerAttributeOperation {
     Unspec,
     #[nla_type(crate::ZERO)]
     Id(u32),
+    #[nla_type(_)]
+    Unknown(UnknownAttribute),
 }
 
 fn main() {}
--- netlink15_derive/tests/ui/deserializable/nla-type-overflow-fails.rs
+++ netlink15_derive/tests/ui/deserializable/nla-type-overflow-fails.rs
@@ -1,6 +1,7 @@
 use netlink15_derive::NetlinkAttributeDeserializable;
 use netlink15_derive::NetlinkAttributeSerializable;
 use netlink15_core::utils::ParseNlaIntError;
+use netlink15_core::attr::UnknownAttribute;
 
 #[derive(Debug, PartialEq, NetlinkAttributeSerializable, NetlinkAttributeDeserializable)]
 #[netlink15(deserialize(error = "ParseNlaIntError"))]
@@ -9,6 +10,8 @@ enum ControllerAttributeOperation {
     Unspec,
     #[nla_type(u16::MAX + 1)]
     Id(u32),
+    #[nla_type(_)]
+    Unknown(UnknownAttribute),
 }
 
 fn main() {}
